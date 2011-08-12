8c1d995e0e7f48a57a377bd910d23d0e166bd510
Andrei Z <b1naryth1ef@gmail.com>
2011-08-12 01:18:17 -0500

    Clear out for v2.0
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- test.py
+++
@@ -1,6 +0,0 @@
-from pyquake3 import PyQuake3
-
-Q = PyQuake3('localhost:27960', rcon_password='123abc')
-Q.rcon_update()
-for i in Q.players:
-	print i.name, i.uid
--- tools.py
+++
@@ -1,9 +0,0 @@
-def error(code,err):
-	if code == "0":
-		print "CRITICAL: "+err
-	elif code == "1":
-		print "ERROR: "+err
-	elif code == "00":
-		print "MOD CRITICAL: "+err
-	elif code == "01":
-		print "MOD ERROR: "+err
