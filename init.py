14337068a7427c28ad78392c59eaa26c0fab9158
Andrei Z <b1naryth1ef@gmail.com>
2011-11-12 22:16:15 -0600

    Moar...

--- init.py
+++ init.py
@@ -159,8 +159,10 @@ def parse(inp):
 		if inp[2].startswith('!'):
 			BOT.eventFire('CLIENT_COMMAND', {'event':'CHAT_MESSAGE', 'sender':inp[1], 'gid':inp[0], 'msg':inp[2]})
 			print BOT.Commands, inp[2].split(' ')[0]
-			if inp[2].split(' ')[0] in BOT.Commands:
+			if inp[2].split(' ')[0] in BOT.Commands.keys():
 				BOT.Commands[inp[2].split(' ')[0]](0) #@TEMP 0 should become chat object
+			else:
+				BOT.Commands['!help'](0)
 		BOT.eventFire('CHAT_MESSAGE', {'event':'CHAT_MESSAGE', 'sender':inp[1], 'gid':inp[0], 'msg':inp[2]})
 
 	elif inp.startswith('ClientConnect:'):
