22fd82ec7dc0d9c5cb91e88c9649bf9dc947f14e
Andrei Z <b1naryth1ef@gmail.com>
2011-11-23 14:08:32 -0600

    3
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- mods/flagstats.py
+++ mods/flagstats.py
@@ -1,23 +1,57 @@
+import time
 api = None
 
 _name = "Flag Stats"
 _author = "B1naryth1ef"
 _version = 0.1
 
+class Timer(object):
+	def __init__(self):
+		self.start = 0
+		self.end = 0
+		self.status = 0
+	
+	def start(self): 
+		if self.status == 0:
+			self.start = time.time()
+			self.status = 1
+	def stop(self):
+		if self.status == 1: 
+			self.end = time.time()
+			self.status = 0
+	def value(self): return self.end-self.start
+	def reset(self):
+		self.start = 0
+		self.end = 0
+		self.status = 0
+
 def eventListener(obj):
-	print "[][][][][][][][]"
+	global api
 	if obj == 'init':
-		pass #setup stuff
+		redFlag = Timer()
+		blueFlag = Timer()
+
 	elif obj.type == "GAME_FLAGPICKUP":
-		print obj.data['flag'], obj.data['team']
-	elif obj.type == "GAME_FLAGDROP":
-		print obj.data['flag'], obj.data['flagid']
+		if obj.data['flagid'] == 1 and redFlag.status == 0: redFlag.start()
+		elif obj.data['flagid'] == 2 and blueFlag.status == 0: blueFlag.start()
+
 	elif obj.type == "GAME_FLAGRETURN":
-		print obj.data['flag'], obj.data['flagid']
+		if obj.data['flagid'] == 1: redFlag.reset()
+		elif obj.data['flagid'] == 2: blueFlag.reset()
+
 	elif obj.type == "GAME_FLAGCAPTURE":
-		print obj.data['flag'], obj.data['flagid']
+		if obj.data['flagid'] == 1:
+			redFlag.stop()
+			api.say('%sRed %sFlag captured in %s%s' % (api.RED, api.YELLOW, api.CYAN ,redFlag.value()))
+		elif obj.data['flagid'] == 2:
+			blueFlag.stop()
+			api.say('%sBlue %sFlag captured in %s%s' % (api.BLUE, api.YELLOW, api.CYAN ,redFlag.value()))
+	elif obj.type == "GAME_FLAGRESET":
+		if obj.data['flagid'] == 1: redFlag.reset()
+		elif obj.data['flagid'] == 2: blueFlag.reset()
 
 def init(A):
 	global api
 	api = A
-	api.addListeners(['GAME_FLAGPICKUP', 'GAME_FLAGDROP', 'GAME_FLAGRETURN', 'GAME_FLAGCAPTURE'], eventListener)
+	eventListener('init')
+	api.addListeners(['GAME_FLAGPICKUP', 'GAME_FLAGDROP', 'GAME_FLAGRETURN', 'GAME_FLAGCAPTURE', 'GAME_FLAGRESET'], eventListener)
