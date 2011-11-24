d3ca4cf02356e25c96931ef722381b3c3619e68e
Andrei Z <b1naryth1ef@gmail.com>
2011-11-23 21:35:48 -0600

    Temp changes for testing

--- const.py
+++ const.py
@@ -66,66 +66,66 @@ itemr = {
 'UT_MOD_FLAG': {'id':'39'},
 'UT_MOD_GOOMBA': {'id':'40'}}
 
-damage = {
-	'PASSIVE': [0, 0, 0, 0, 0, 0, 0, 0],
-    MOD_TELEFRAG['id']: [0, 0, 0, 0, 0, 0, 0, 0],
-    UT_MOD_KNIFE['id']: [100, 60, 44, 35, 20, 20, 44, 100],
-    UT_MOD_KNIFE_THROWN['id']: [100, 60, 44, 35, 20, 20, 44, 100],
-    UT_MOD_BERETTA['id']: [100, 34, 30, 20, 11, 11, 30, 100],
-    UT_MOD_DEAGLE['id']: [100, 66, 57, 38, 22, 22, 57, 100],
-    UT_MOD_SPAS['id']: [25, 25, 25, 25, 25, 25, 25, 100],
-    UT_MOD_UMP45['id']: [100, 51, 44, 29, 17, 17, 44, 100],
-    UT_MOD_MP5K['id']: [50, 34, 30, 20, 11, 11, 30, 100],
-    UT_MOD_LR300['id']: [100, 51, 44, 29, 17, 17, 44, 100],
-    UT_MOD_G36['id']: [100, 51, 44, 29, 17, 17, 44, 100],
-    UT_MOD_PSG1['id']: [100, 63, 97, 63, 36, 36, 97, 100],
-    UT_MOD_HK69['id']: [50, 50, 50, 50, 50, 50, 50, 100],
-    UT_MOD_BLED['id']: [15, 15, 15, 15, 15, 15, 15, 15],
-    UT_MOD_KICKED['id']: [20, 20, 20, 20, 20, 20, 20, 100],
-    UT_MOD_HEGRENADE['id']: [50, 50, 50, 50, 50, 50, 50, 100],
-    UT_MOD_SR8['id']: [100, 100, 100, 100, 50, 50, 100, 100],
-    UT_MOD_AK103['id']: [100, 58, 51, 34, 19, 19, 51, 100],
-    UT_MOD_NEGEV['id']: [50, 34, 30, 20, 11, 11, 30, 100],
-    UT_MOD_HK69_HIT['id']: [20, 20, 20, 20, 20, 20, 20, 100],
-    UT_MOD_M4['id']: [100, 51, 44, 29, 17, 17, 44, 100],
-    UT_MOD_GOOMBA['id']: [100, 100, 100, 100, 100, 100, 100, 100],
-    }
+# damage = {
+# 	'PASSIVE': [0, 0, 0, 0, 0, 0, 0, 0],
+#     MOD_TELEFRAG['id']: [0, 0, 0, 0, 0, 0, 0, 0],
+#     UT_MOD_KNIFE['id']: [100, 60, 44, 35, 20, 20, 44, 100],
+#     UT_MOD_KNIFE_THROWN['id']: [100, 60, 44, 35, 20, 20, 44, 100],
+#     UT_MOD_BERETTA['id']: [100, 34, 30, 20, 11, 11, 30, 100],
+#     UT_MOD_DEAGLE['id']: [100, 66, 57, 38, 22, 22, 57, 100],
+#     UT_MOD_SPAS['id']: [25, 25, 25, 25, 25, 25, 25, 100],
+#     UT_MOD_UMP45['id']: [100, 51, 44, 29, 17, 17, 44, 100],
+#     UT_MOD_MP5K['id']: [50, 34, 30, 20, 11, 11, 30, 100],
+#     UT_MOD_LR300['id']: [100, 51, 44, 29, 17, 17, 44, 100],
+#     UT_MOD_G36['id']: [100, 51, 44, 29, 17, 17, 44, 100],
+#     UT_MOD_PSG1['id']: [100, 63, 97, 63, 36, 36, 97, 100],
+#     UT_MOD_HK69['id']: [50, 50, 50, 50, 50, 50, 50, 100],
+#     UT_MOD_BLED['id']: [15, 15, 15, 15, 15, 15, 15, 15],
+#     UT_MOD_KICKED['id']: [20, 20, 20, 20, 20, 20, 20, 100],
+#     UT_MOD_HEGRENADE['id']: [50, 50, 50, 50, 50, 50, 50, 100],
+#     UT_MOD_SR8['id']: [100, 100, 100, 100, 50, 50, 100, 100],
+#     UT_MOD_AK103['id']: [100, 58, 51, 34, 19, 19, 51, 100],
+#     UT_MOD_NEGEV['id']: [50, 34, 30, 20, 11, 11, 30, 100],
+#     UT_MOD_HK69_HIT['id']: [20, 20, 20, 20, 20, 20, 20, 100],
+#     UT_MOD_M4['id']: [100, 51, 44, 29, 17, 17, 44, 100],
+#     UT_MOD_GOOMBA['id']: [100, 100, 100, 100, 100, 100, 100, 100],
+#     }
 
-gearInfo = {
-	#Sidearms
-	'F':{'id':UT_MOD_BERETTA['id'], 'name':'beretta', 'damage':damage[UT_MOD_BERETTA['id']]},
-	'G':{'id':UT_MOD_DEAGLE['id'],'name':'desert eagle', 'damage':damage[UT_MOD_DEAGLE['id']]},
+# gearInfo = {
+# 	#Sidearms
+# 	'F':{'id':UT_MOD_BERETTA['id'], 'name':'beretta', 'damage':damage[UT_MOD_BERETTA['id']]},
+# 	'G':{'id':UT_MOD_DEAGLE['id'],'name':'desert eagle', 'damage':damage[UT_MOD_DEAGLE['id']]},
 
-	#Primary
-	'K':{'id':UT_MOD_HK69['id'], 'name':'hk69', 'damage': damage[UT_MOD_HK69['id']]},
-	'L':{'id':UT_MOD_LR300['id'], 'name':'lr300', 'damage': damage[UT_MOD_LR300['id']]},
-	'M':{'id':UT_MOD_G36['id'], 'name':'g36', 'damage': damage[UT_MOD_G36['id']]},
-	'N':{'id':UT_MOD_PSG1['id'], 'name':'psg1','damage': damage[UT_MOD_PSG1['id']]},
-	'Z':{'id':UT_MOD_SR8['id'],'name':'sr8','damage': damage[UT_MOD_SR8['id']]},
-	'a':{'id':UT_MOD_AK103['id'],'name':'ak103','damage': damage[UT_MOD_AK103['id']]},
-	'c':{'id':UT_MOD_NEGEV['id'],'name':'negav','damage': damage[UT_MOD_NEGEV['id']]},
-	'e':{'id':UT_MOD_M4['id'],'name':'m4','damage': damage[UT_MOD_M4['id']]},
+# 	#Primary
+# 	'K':{'id':UT_MOD_HK69['id'], 'name':'hk69', 'damage': damage[UT_MOD_HK69['id']]},
+# 	'L':{'id':UT_MOD_LR300['id'], 'name':'lr300', 'damage': damage[UT_MOD_LR300['id']]},
+# 	'M':{'id':UT_MOD_G36['id'], 'name':'g36', 'damage': damage[UT_MOD_G36['id']]},
+# 	'N':{'id':UT_MOD_PSG1['id'], 'name':'psg1','damage': damage[UT_MOD_PSG1['id']]},
+# 	'Z':{'id':UT_MOD_SR8['id'],'name':'sr8','damage': damage[UT_MOD_SR8['id']]},
+# 	'a':{'id':UT_MOD_AK103['id'],'name':'ak103','damage': damage[UT_MOD_AK103['id']]},
+# 	'c':{'id':UT_MOD_NEGEV['id'],'name':'negav','damage': damage[UT_MOD_NEGEV['id']]},
+# 	'e':{'id':UT_MOD_M4['id'],'name':'m4','damage': damage[UT_MOD_M4['id']]},
 
-	#Primary and Secondary
-	'H':{'id':UT_MOD_SPAS['id'],'name':'spas', 'damage': damage[UT_MOD_SPAS['id']]},
-	'I':{'id':UT_MOD_MP5K['id'],'name':'mp5k', 'damage': damage[UT_MOD_MP5K['id']]},
-	'J':{'id':UT_MOD_UMP45['id'],'name':'ump45', 'damage': damage[UT_MOD_UMP45['id']]},
+# 	#Primary and Secondary
+# 	'H':{'id':UT_MOD_SPAS['id'],'name':'spas', 'damage': damage[UT_MOD_SPAS['id']]},
+# 	'I':{'id':UT_MOD_MP5K['id'],'name':'mp5k', 'damage': damage[UT_MOD_MP5K['id']]},
+# 	'J':{'id':UT_MOD_UMP45['id'],'name':'ump45', 'damage': damage[UT_MOD_UMP45['id']]},
 
-	#Grenades
-	'O':{'id':UT_MOD_HEGRENADE['id'], 'name':'he grenade', 'damage': damage[UT_MOD_HEGRENADE['id']]},
-	'Q':None, #{'id':None,'name':'smoke grenade','damage': damage['PASSIVE']}, #Smoke nade
+# 	#Grenades
+# 	'O':{'id':UT_MOD_HEGRENADE['id'], 'name':'he grenade', 'damage': damage[UT_MOD_HEGRENADE['id']]},
+# 	'Q':None, #{'id':None,'name':'smoke grenade','damage': damage['PASSIVE']}, #Smoke nade
 
-	#Items
-	'R':None, #Kevlar vest
-	'S':None, #TAC Goggles
-	'T':None, #Medkit
-	'Y':None, #Silencer
-	'V':None, #laser sight
-	'W':None, #kevlar helmet
-	'X':None, #extra ammo
+# 	#Items
+# 	'R':None, #Kevlar vest
+# 	'S':None, #TAC Goggles
+# 	'T':None, #Medkit
+# 	'Y':None, #Silencer
+# 	'V':None, #laser sight
+# 	'W':None, #kevlar helmet
+# 	'X':None, #extra ammo
 
-	'A':None #None :D
-}
+# 	'A':None #None :D
+# }
 
 
 class UrTBotError(Exception): pass
