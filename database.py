2bbd750861c90d4a17a87a65c532a2c3fe3a095d
spekode <jeffroberts1@gmail.com>
2011-11-25 15:22:28 -0800

    Running database.py now creates the basic tables

--- database.py
+++ database.py
@@ -9,22 +9,37 @@ class DB(db_plugin.DBPlugin):
 		db_plugin.DBPlugin.__init__(self)
 		self.connect(dbConfig)
 
-	def clientAdd(): pass
-	def clientDel(): pass
-	def clientModify(): pass
-	def clientSearch(field, value): pass
+	def clientAdd(self): pass
+	def clientDel(self): pass
+	def clientModify(self): pass
+	def clientSearch(self, field, value): pass
 
-	def clientSelect(): pass
+	def clientSelect(self): pass
 
-	def aliasAdd(): pass
-	def aliasDel(): pass
-	def aliasSearch(): pass
-	def aliasSelect(): pass
+	def aliasAdd(self): pass
+	def aliasDel(self): pass
+	def aliasSearch(self): pass
+	def aliasSelect(self): pass
 
-	def penaltyAdd(): pass
-	def penaltyDel(): pass
-	def penaltyModify(): pass
-	def penaltySearch(): pass
-	def penaltySelect(): pass
+	def penaltyAdd(self): pass
+	def penaltyDel(self): pass
+	def penaltyModify(self): pass
+	def penaltySearch(self): pass
+	def penaltySelect(self): pass
 
-	def defaultTableSet(): pass
+	def defaultTableSet(self):
+		self.addTable('clients', {'id':'integer', 'cgroup':'integer', 'nick':'text',
+		'guid':'text', 'password':'text', 'ip':'text', 'lastip':'text'})
+
+		self.addTable('penalties', {'id':'integer', 'userid':'integer',
+		'adminid':'integer', 'type':'text', 'time':'integer', 'expiration':'integer'})
+		self.commit()
+
+if __name__ == '__main__':
+	db = DB()
+	print "Making default tables..."
+	try:
+		db.defaultTableSet()
+	except Exception, e:
+		print "Ruh roh! Failed because %s." % e
+		sys.exit()
