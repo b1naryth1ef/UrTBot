055a74017d96761955447084dbe1dc453e0dd88c
Andrei Z <b1naryth1ef@gmail.com>
2011-12-21 13:56:21 -0600

    So I guess we do need a second variable...

--- mods/default.py
+++ mods/default.py
@@ -31,11 +31,11 @@ class Timer(object): #@CREDIT B1
 		self.status = 0
 
 @command('!tt', 'Test Plugin', 0)
-def tester(obj, **args):
+def tester(obj, t):
 	print 'Test!'
 
 @command('!help', 'List all commands, or info on a specific command. Usage: !help <cmd>', 0)
-def cmdHelp(obj): #@CREDIT Neek
+def cmdHelp(obj, t): #@CREDIT Neek
 	#format should be !command : Info \n
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
@@ -64,12 +64,12 @@ def cmdHelp(obj): #@CREDIT Neek
 			A.tell(sender, "%s: %s" % (cmd, cmdobj[1]))
 
 @command('!about', 'About UrTBot', 0)
-def cmdAbout(obj):
+def cmdAbout(obj, t):
 	sender = obj.data["sender"]
 	A.tell(sender, "UrTBot: V%s by Neek and B1naryth1ef" % __Version__)
 
 @command('!list', 'List all users. Usage: !list', 3)
-def cmdList(obj):
+def cmdList(obj, t):
 	sender = obj.data["sender"]
 	A.tell(sender, "==Player List==")
 	clients = A.getClients()
@@ -77,7 +77,7 @@ def cmdList(obj):
 		A.tell(sender, "[%2d] %s (%s)" % (cid, clients[cid].name, clients[cid].ip))
 
 @command('!kick', 'Kick a user. Usage: !kick <NAME/UID>', 3)
-def cmdKick(obj):
+def cmdKick(obj, t):
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 	if len(msg) == 1: A.tell(sender, "Usage: !kick <user>")
@@ -88,7 +88,7 @@ def cmdKick(obj):
 		A.rcon('clientkick %d' % cid)
 
 @command('!slap', 'Slap a player. Usage: !slap <NAME/UID>', 3)
-def cmdSlap(obj):
+def cmdSlap(obj, t):
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 	if len(msg) == 1: A.tell(sender, "Usage: !slap <user> <count>")
@@ -104,7 +104,7 @@ def cmdSlap(obj):
 			time.sleep(.5) #@NOTE Seems like a good delay time
 
 @command('!nuke', 'Nuke a player. Usage: !nuke <NAME/UID>', 3)
-def cmdNuke(obj):
+def cmdNuke(obj, t):
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 	if len(msg) == 1: A.tell(sender, "Usage: !nuke <user> <count>")
@@ -120,7 +120,7 @@ def cmdNuke(obj):
 			time.sleep(.5) #@NOTE Seems like a good delay time
 
 @command('!set', 'Set a Q3 Variable. Usage: !set <cvar> <value>', 5)
-def cmdSet(obj): 
+def cmdSet(obj, t): 
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 	if len(msg) == 1: A.tell(sender, "Usage: !set <cvar> <value>")
@@ -128,7 +128,7 @@ def cmdSet(obj):
 		A.rcon('set %s %s' % (msg[1], msg[2]))
 
 @command('!map', 'Load a map. Usage: !map <map>', 2)
-def cmdMap(obj):
+def cmdMap(obj, t):
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 	if len(msg) == 1: A.tell(sender, "Usage: !map <map>")
@@ -143,11 +143,11 @@ def cmdMap(obj):
 			A.rcon('vstr thismap')
 
 @listener('CLIENT_BEGIN')
-def welcomeEvent(obj):
+def welcomeEvent(obj, t):
 	if obj.type == 'CLIENT_BEGIN': A.say('Everyone welcome ^1%s ^3to the server!' % A.B.Clients[obj.data['client']].name)
 
 @command('!timer', 'Start/stop the timer. Usage: !timer', 1)
-def cmdTime(obj):
+def cmdTime(obj, t):
 	global TIMERZ
 	sender = obj.data['sender']
 	if sender in TIMERZ:
@@ -163,7 +163,7 @@ def cmdTime(obj):
 		TIMERZ[sender].start()
 		A.tell(sender, 'Timer Started!')
 
-def cmdIDDQD(obj):
+def cmdIDDQD(obj, t):
 	sender = obj.data['sender']
 	client = A.getClient(sender)
 
--- mods/fairplay.py
+++ mods/fairplay.py
@@ -2,7 +2,7 @@ from init import A, command, listener
 import random, const
 
 @command('!teams', "Attempt to balance uneven teams", 1)
-def cmdTeams(obj):
+def cmdTeams(obj, t):
 	msg = obj.data["msg"].split(" ")
 	sender = obj.data["sender"]
 
@@ -35,7 +35,7 @@ def cmdTeams(obj):
 	A.say("Balanced teams.") #<<< should be told to the user?
 
 @listener('CLIENT_SWITCHTEAM')
-def eveLock(obj):
+def eveLock(obj, t):
 	cobj = A.getClient(obj.client)
 	if 'fairplay_locked' in cobj.__dict__.keys():
 		if cobj.fairplay_locked is True:
@@ -43,7 +43,7 @@ def eveLock(obj):
 
 @command('!lock', 'Lock a player to the team they are on. Usage: !lock <player>', 3)
 @command('!unlock', 'Unlock a locked player. Usage: !unlock <player>', 3)
-def cmdLock(obj):
+def cmdLock(obj, t):
 	#![un]lock user
 
 	if len(obj.msgsplit) != 2: return A.tell(obj.sender, "Usage: %s <client>" % obj.cmd) #Sneaky bastard that I am
@@ -65,7 +65,7 @@ def cmdLock(obj):
 		A.tell(obj.sender, 'Success! %s was unlocked!' % (playobj.name))
 
 @command('!force', 'Force a player, and lock him if need be! Usage: !force <player> <team> [lock]')
-def cmdForce(obj):
+def cmdForce(obj, t):
 	msg = obj.data['msgsplit']
 	sender = obj.data['sender'] #The sender id
 	team = msg[2] #Team to switch player to
