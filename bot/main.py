b6ab5db34c73fe4cd2c2064c748a6ef00a4b7c91
Andrei Z <b1naryth1ef@gmail.com>
2012-08-04 16:28:58 -0500

    Debug to fix weird issue

--- bot/main.py
+++ bot/main.py
@@ -126,7 +126,9 @@ def parseClientDisconnect(inp): #ClientDisconnect: 0
 def parseKill(inp):
     #Kill: 1 0 15: WolfXxXBunny killed [WoC]*B1naryth1ef by UT_MOD_DEAGLE
     inp = inp.split(" ")[1:]
-    if int(inp[0]) == 1022: atk = None #We're world.
+    if int(inp[0]) == 1022: 
+        log.debug('We\'re world!')
+        atk = None #We're world.
     else: atk = BOT.getClient(int(inp[0])) #We're a player
     vic = BOT.getClient(int(inp[1]))
     method = int(inp[2][:-1])
